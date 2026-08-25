7885404dc9960d3c40a6e8138719d2953ca46ca5
Ilya Haliankou <ilya.haliankou@cerifi.com>
2026-08-25 17:05:11 +0200

    feat(contracts): add the package with the prediction market list

---
+++ packages/contracts/src/index.ts
@@ -0,0 +1,7 @@
+export type { ImplementedMarket, PredictionMarket } from "./markets/market.js";
+export {
+  IMPLEMENTED_MARKETS,
+  implementedMarketSchema,
+  PREDICTION_MARKETS,
+  predictionMarketSchema,
+} from "./markets/market.js";
